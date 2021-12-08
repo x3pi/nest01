df47c06625d25e8b4626c8a27aeeeedb864dc889
x3pi <45082550+x3pi@users.noreply.github.com>
2021-12-09 00:11:34 +0700

    add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -10,6 +12,18 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter()
   );
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
