a2d5f13aed6a52155f49ff7a6c62b47885db9bde
x3pi <45082550+x3pi@users.noreply.github.com>
2021-12-08 22:30:51 +0700

    add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware, LoggerMiddleware2} from './common/middleware/logger.middleware';
+
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, LoggerMiddleware2)
+      .forRoutes('cats');
+  }
+}
